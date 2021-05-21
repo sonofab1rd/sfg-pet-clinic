2f0ec44de515ed18ade26f649f3f5a98f86400a6
sonofab1rd <jacobdierkens@jacobs-mbp-2.neo.rr.com>
2021-05-21 13:43:41 -0400

    init setup

---
+++ src/test/java/jake/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jake.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
