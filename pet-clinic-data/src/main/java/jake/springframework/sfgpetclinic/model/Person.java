eb4c745b84f5d9dc43deff84043791c0261079e6
Jacob Dierkens <westernreserve@gmail.com>
2021-05-23 09:37:39 -0400

    Add id to model objects. Closes #10.

--- pet-clinic-data/src/main/java/jake/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/jake/springframework/sfgpetclinic/model/Person.java
@@ -1,23 +1,23 @@
 package jake.springframework.sfgpetclinic.model;
 
-public class Person {
-	private String firstName;
-	private String lastName;
+public class Person extends BaseEntity {
+    private String firstName;
+    private String lastName;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
