3a9bd891f101909fddce72566dec801901c1862a
Jacob Dierkens <westernreserve@gmail.com>
2021-05-28 18:03:31 -0400

    create Specialty service map Closes #31

---
+++ pet-clinic-data/src/main/java/jake/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package jake.springframework.sfgpetclinic.services;
+
+import jake.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/jake/springframework/sfgpetclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package jake.springframework.sfgpetclinic.services.map;
+
+import jake.springframework.sfgpetclinic.model.Specialty;
+import jake.springframework.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty getById(Long id) {
+        return super.findById(id);
+    }
+}
